d6df470d19d594a47a01bd973a58304c9ec842e0
PastLeo <chgu82837@gmail.com>
2023-10-30 23:41:36 +0800

    publish 0.1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HandsontableKinoSmartcell.MixProject do
   def project do
     [
       app: :handsontable_kino_smartcell,
-      version: "0.1.4",
+      version: "0.1.5",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
